81029e2e8eafdd92a0766d5a83eb764cab83a37c
Đào Văn Đạt <daovandat2k15@gmail.com>
2023-07-11 13:53:56 +0700

    Create Todo Logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todo } from "./Type/model";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
-  const [todo, setTodo] = useState<string>("");
+  const [todoSearch, setTodoSearch] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todoSearch) {
+      setTodos([...todos, { id: Date.now(), todo: todoSearch, isDone: false }]);
+      setTodoSearch("");
+    }
+  };
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField
+        todoSearch={todoSearch}
+        setTodoSearch={setTodoSearch}
+        handleAdd={handleAdd}
+      />
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./style.css";
 
 interface Props {
-  todo: string;
-  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  todoSearch: string;
+  setTodoSearch: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({
+  todoSearch,
+  setTodoSearch,
+  handleAdd,
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef?.current?.blur();
+      }}
+    >
       <input
+        ref={inputRef}
         type="input"
         placeholder="Enter a task"
         className="input__box"
-        value={todo}
-        onChange={(e) => setTodo(e.target.value)}
+        value={todoSearch}
+        onChange={(e) => setTodoSearch(e.target.value)}
       />
       <button type="submit" className="input__submit">
         Go
