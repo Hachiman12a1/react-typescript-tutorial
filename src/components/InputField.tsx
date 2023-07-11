58f456c451a8ec5d07a877c3b64c26dfe23dc574
Đào Văn Đạt <daovandat2k15@gmail.com>
2023-07-11 10:55:06 +0700

    Add Use State To Input Typescript

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./style.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+      <input
+        type="input"
+        placeholder="Enter a task"
+        className="input__box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
       <button type="submit" className="input__submit">
         Go
       </button>
